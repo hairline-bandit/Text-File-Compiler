1ca99b83ea8d44f849e6de59159eaf5440e8084f
hairline-bandit <77307697+hairline-bandit@users.noreply.github.com>
2021-04-08 08:38:56 -0500

    Add files via upload

---
+++ src/script.ps1
@@ -0,0 +1,10 @@
+param ($language, $file, $file2)
+
+if ( $language = "python"){
+	New-Item -Path "C:\users\$env:UserName\desktop\$file2" -ItemType File
+	cat "C:\users\$env:UserName\desktop\$file" > "C:\users\$env:UserName\desktop\$file2"
+	$attack="C:\users\$env:UserName\appdata\local\programs\python\python37\python.exe"
+	$fil="C:\users\$env:UserName\desktop\$file2"
+	Start-Process -FilePath $attack -ArgumentList $fil
+}
+
