5cfc032368d98786de86d971511aaf475a9f2571
hairline-bandit <77307697+hairline-bandit@users.noreply.github.com>
2021-04-09 08:08:46 -0500

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -20,4 +20,30 @@ if ( $language -eq "python"){
 	Start-Process -FilePath "C:\users\$env:UserName\desktop\a.exe" -Wait -NoNewWindow
 	Remove-Item -path $fil
 	Remove-Item -path "C:\users\$env:UserName\desktop\a.exe"	
+} elseif ( $language -eq "cpp"){
+	$name="11111111111111111111111111111111.cpp"
+	New-Item -Path "C:\users\$env:UserName\desktop\$name" -ItemType File
+	cat "C:\users\$env:UserName\desktop\$file" > "C:\users\$env:UserName\desktop\$name"
+	$attack="g++"
+	$fil="C:\users\$env:UserName\desktop\$name"
+	"+++++++++++++++++++EXECUTING CODE++++++++++++++++++++"
+	Start-Process $attack -ArgumentList $fil -Wait -NoNewWindow
+	Start-Process -FilePath "C:\users\$env:UserName\desktop\a.exe" -Wait -NoNewWindow
+	Remove-Item -path $fil
+	Remove-Item -path "C:\users\$env:UserName\desktop\a.exe"
+} elseif ( $language -eq "java"){
+	$prefix=$file.Substring(0, $file.length - 4)
+	$name= -join($prefix, ".java")	
+	New-Item -Path "C:\users\$env:UserName\desktop\$name" -ItemType File
+	cat "C:\users\$env:UserName\desktop\$file" > "C:\users\$env:UserName\desktop\$name"
+	$attack="javac"
+	$attacketc="java"
+	$fil="C:\users\$env:UserName\desktop\$name"
+	"+++++++++++++++++++EXECUTING CODE++++++++++++++++++++"
+	Start-Process $attack -ArgumentList $fil -Wait -NoNewWindow
+	Start-Process $attacketc -ArgumentList $prefix -Wait -NoNewWindow
+	Remove-Item -path $fil
+	$removal= -join($prefix, ".class")
+	Remove-Item -path "C:\users\$env:UserName\desktop\$removal"
 }
+
